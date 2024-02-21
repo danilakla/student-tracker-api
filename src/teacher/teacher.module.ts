680db052b8d0ef66407edbaca3ffeefc16137444
Danila <67117334+danilakla@users.noreply.github.com>
2024-02-21 16:31:15 +0300

    start implementation of teacher module

--- src/teacher/teacher.module.ts
+++ src/teacher/teacher.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TeacherController } from './teacher.controller';
 import { TeacherService } from './teacher.service';
+import { SubjectModule } from 'src/subject/subject.module';
 
 @Module({
+  imports:[SubjectModule],
   controllers: [TeacherController],
   providers: [TeacherService]
 })
--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -1,4 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { SubjectDto } from 'src/subject/dto/subject.dto';
+import { SubjectService } from 'src/subject/subject.service';
 
 @Injectable()
-export class TeacherService {}
+export class TeacherService {
+    constructor(private subjectSerivce:SubjectService){
+
+    }
+
+
+   async createSubject(subjectId:SubjectDto, teacherId: number){
+    try {
+       return await this.subjectSerivce.addSuject(subjectId, teacherId);
+        
+    } catch (error) {
+    throw error;        
+    }    
+   }
+
+   async updateSubject(subjectId:SubjectDto, teacherId: number){
+    try {
+       return await this.subjectSerivce.updateSubject(subjectId, teacherId);
+        
+    } catch (error) {
+    throw error;        
+    }    
+   }
+
+   async deleteSubject(subjectId: number, teacherId: number){
+    try {
+       return await this.subjectSerivce.deleteSubject(subjectId, teacherId);
+        
+    } catch (error) {
+    throw error;        
+    }    
+   }
+
+
+   async getOneSubject(subjectId: number, teacherId: number){
+    try {
+       return await this.subjectSerivce.getSubjectByTeacherSubjectId(subjectId, teacherId);
+        
+    } catch (error) {
+    throw error;        
+    }    
+   }
+
+   
+   async geManySubjects(subjectId: number, teacherId: number){
+    try {
+       return await this.subjectSerivce.getSubjects( teacherId);
+        
+    } catch (error) {
+    throw error;        
+    }    
+}
+}
