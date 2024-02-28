8ec3903ba5c102fd7b8f5a71dd63af65ac411371
Danila <67117334+danilakla@users.noreply.github.com>
2024-02-28 19:36:47 +0300

    configure https protocole to allow webRTC

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import *  as fs from 'fs';
+const  httpsOptions={
+  key:fs.readFileSync('D:\\key.pem'),
+  cert:fs.readFileSync('D:\\cert.pem')
+}
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {httpsOptions});
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
