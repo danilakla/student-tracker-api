4e6c38aa86e0f10c3c7bde6db12109249581c311
Danila <67117334+danilakla@users.noreply.github.com>
2024-10-17 22:03:43 +0300

    run https

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,22 @@
-FROM node:20.18.0
+FROM node:18
 WORKDIR /app
-COPY . .
+
+COPY package*.json ./
+RUN apt update && apt install -y build-essential python3 libtool autoconf automake
+
+RUN rm -rf node_modules package-lock.json
+
 RUN npm install
+RUN npm rebuild argon2
+
+COPY . .
+
+
 RUN npx prisma generate
+
+RUN npm run build
+
+
 EXPOSE 3333
-CMD ["npm", "run", "start:dev"]
+
+CMD ["npm", "run", "start:prod"]
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import *  as fs from 'fs';
 const  httpsOptions={
-  key:fs.readFileSync('./key.pem'),
-  cert:fs.readFileSync('./cert.pem')
+  key:fs.readFileSync('key.pem'),
+  cert:fs.readFileSync('cert.pem')
 }
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {httpsOptions});
+  const app = await NestFactory.create(AppModule,{httpsOptions});
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+  app.setGlobalPrefix('api');
+
   await app.listen(3333);
 }
 bootstrap();
